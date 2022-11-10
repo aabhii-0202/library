70d98d492e87b039df04d0ad25c76560f4aa989e
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-11-11 01:58:07 +0530

    Css in list view

--- src/components/List.js
+++ src/components/List.js
@@ -6,17 +6,29 @@ export default function List({books}) {
     <div className='list'>
         {
           books.map(item => {
-              return (
-                  <div className='div'>
-                    <img className='image' src={item.image} alt="img" />
-                    <div>
-                    <text className='name'>{item.name}</text>
-                    <text className='author'>{item.author}</text>
-                    <text className='subject'>{item.subject}</text>
-                    <text className='date'>{item.date}</text>
-                    </div>
+            return (
+              <div className='div'>
+                <img className='image' src={item.image} alt="img" />
+                <div className='div3'>
+                  <div className='div2'>
+                    <text className='lable'>Name:</text>
+                    <text className='value'>{item.name}</text>
                   </div>
-              );
+                  <div className='div2'>
+                    <text className='lable'>Author's name:</text>
+                    <text className='value'>{item.author}</text>
+                  </div>
+                  <div className='div2'>
+                    <text className='lable'>Subject:</text>
+                    <text className='value'>{item.subject}</text>
+                  </div>
+                  <div className='div2'>
+                    <text className='lable'>Date Of Publish:</text>
+                    <text className='value'>{item.date}</text>
+                  </div>
+                </div>
+              </div>
+            );
           })
         }
     </div>
